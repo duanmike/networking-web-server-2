d3c14df18998f07ad117c26a95d852f1f6b3ff7c
Michael Duan <69947599+duanmike@users.noreply.github.com>
2020-09-29 21:53:32 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -43,7 +43,6 @@ def webServer(port=13331):
             #Close client socket
             #Fill in start
             connectionSocket.close()
-            serverSocket.close()
             #Fill in end
 
     serverSocket.close()
